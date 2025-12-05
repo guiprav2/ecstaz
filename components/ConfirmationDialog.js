56f153d8cc25464d2df18be391b356a872070f7c
Camila Prá <camila@guiprav.com>
2025-12-04 23:28:03 -0300

    .

---
+++ components/ConfirmationDialog.js
@@ -0,0 +1,4 @@
+export default class ConfirmationDialog {
+	constructor(props) { this.props = props }
+  toggleAnimate(x) { this.animate = x;  d.update() }
+};
---
+++ components/NewPageDialog.js
@@ -0,0 +1,4 @@
+export default class NewPageDialog {
+	constructor(props) { this.props = props }
+  toggleAnimate(x) { this.animate = x; d.update() }
+};
